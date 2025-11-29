3c7e1cf88bc629d9c4e5f28d77ca461819a53ead
ManaR-Rch <manarmarchou6@gmail.com>
2025-11-29 11:54:45 +0100

    Add WebConfig to register interceptors

---
+++ src/main/java/com/example/smartshop/config/WebConfig.java
@@ -0,0 +1,27 @@
+package com.example.smartshop.config;
+
+import com.example.smartshop.interceptor.AdminInterceptor;
+import com.example.smartshop.interceptor.LoginInterceptor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Component
+public class WebConfig implements WebMvcConfigurer {
+    private LoginInterceptor loginInterceptor;
+    private AdminInterceptor adminInterceptor;
+
+    public WebConfig(LoginInterceptor loginInterceptor, AdminInterceptor adminInterceptor) {
+        this.loginInterceptor = loginInterceptor;
+        this.adminInterceptor = adminInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginInterceptor)
+                .addPathPatterns("/api/client/**", "/api/orders/**", "/api/profile/**");
+
+        registry.addInterceptor(adminInterceptor)
+                .addPathPatterns("/api/admin/**", "/api/clients/**", "/api/products/**", "/api/payments/**");
+    }
+}
