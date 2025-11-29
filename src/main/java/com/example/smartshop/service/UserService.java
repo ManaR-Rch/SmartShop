239b0c39cd4b1318d6a192c122c75d76ded18afc
ManaR-Rch <manarmarchou6@gmail.com>
2025-11-29 11:54:56 +0100

    Update UserService with BCrypt and BusinessRuleViolationException

--- src/main/java/com/example/smartshop/service/UserService.java
+++ src/main/java/com/example/smartshop/service/UserService.java
@@ -1,42 +1,51 @@
 package com.example.smartshop.service;
 
+import com.example.smartshop.config.PasswordUtil;
 import com.example.smartshop.dto.CreateUserDTO;
 import com.example.smartshop.dto.LogInDTO;
 import com.example.smartshop.entity.User;
 import com.example.smartshop.entity.UserRole;
+import com.example.smartshop.exception.BusinessRuleViolationException;
 import com.example.smartshop.mapper.UserMapper;
 import com.example.smartshop.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
-  private UserRepository userRepository;
-  private UserMapper userMapper;
+    private UserRepository userRepository;
+    private UserMapper userMapper;
+    private PasswordUtil passwordUtil;
 
-  public UserService(UserRepository userRepository, UserMapper userMapper) {
-    this.userRepository = userRepository;
-    this.userMapper = userMapper;
-  }
+    public UserService(UserRepository userRepository, UserMapper userMapper, PasswordUtil passwordUtil) {
+        this.userRepository = userRepository;
+        this.userMapper = userMapper;
+        this.passwordUtil = passwordUtil;
+    }
 
-  public User create(CreateUserDTO dto) {
-    User user = userRepository.findByUsername(dto.getUsername()).orElse(null);
-    if (user != null) {
-      throw new RuntimeException("User already exists");
-    }
-    user = userMapper.toEntity(dto);
-    user.setRole(UserRole.CLIENT);
-    userRepository.save(user);
-    return user;
-  }
+    public User create(CreateUserDTO dto) {
+        var existingUser = userRepository.findByUsername(dto.getUsername()).orElse(null);
+        if (existingUser != null) {
+            throw new BusinessRuleViolationException("User already exists");
+        }
 
-  public User logIn(LogInDTO dto) {
-    User user = userRepository.findByUsername(dto.getUsername()).orElse(null);
-    if (user == null) {
-      throw new RuntimeException("Username or password is incorrect");
+        User user = userMapper.toEntity(dto);
+        user.setPassword(passwordUtil.passwordHash(dto.getPassword()));
+        user.setRole(UserRole.CLIENT); // Default role
+
+        userRepository.save(user);
+        return user;
     }
-    if (!user.getPassword().equals(dto.getPassword())) {
-      throw new RuntimeException("Username or password is incorrect");
+
+    public User logIn(LogInDTO dto) {
+        User user = userRepository.findByUsername(dto.getUsername()).orElse(null);
+        if (user == null) {
+            throw new BusinessRuleViolationException("Username or password is incorrect");
+        }
+
+        if (!passwordUtil.checkPassword(dto.getPassword(), user.getPassword())) {
+            throw new BusinessRuleViolationException("Username or password is incorrect");
+        }
+
+        return user;
     }
-    return user;
-  }
 }
